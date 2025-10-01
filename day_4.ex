d28b3866d1b46346ff2f3201d3bf450e122f4974
mason_fertner <masonfertner@gmail.com>
2025-10-01 15:24:57 +0200

    feat(Aoc Day four): Task one
    Finish task one in two ways, using TCO and Stream.

--- day_4.ex
+++ day_4.ex
@@ -1,10 +1,54 @@
-defmodule DayThree do
+defmodule DayFour do
   @moduledoc """
-  Advent of Code 2015 - Day 4: Mining Advent coins.
+  Advent of Code 2015 - Day 4: Mining Advent coins. TCO
   """
 
-  task_data = "bgvyzdsv"
+  # This changes 
+  @task_data "iwrupvqb"
 
+  def check_md5_hash(s, suffix) do
+    hash = :crypto.hash(:md5, s <> Integer.to_string(suffix)) |> Base.encode16(case: :lower)
 
+    if hash |> String.starts_with?("00000") do
+      suffix
+    else
+      check_md5_hash(s, suffix + 1)
+    end
+  end
 
+  def get_md5_suffix(s, suffix) do
+    check_md5_hash(s, suffix)
+  end
+
+  def task_one do
+    get_md5_suffix(@task_data, 1)
+  end
+end
+
+defmodule DayFourStream do
+  @moduledoc """
+  Advent of Code 2015 - Day 4: Mining Advent coins. Stream
+  """
+
+  # This changes 
+  @task_data "iwrupvqb"
+
+  defp find_advent_coin(prefix, required_start \\ "00000") do
+    Stream.iterate(1, &(&1 + 1))
+    |> Stream.map(fn suffix ->
+      hash =
+        :crypto.hash(:md5, prefix <> Integer.to_string(suffix))
+        |> Base.encode16(case: :lower)
+
+      {suffix, hash}
+    end)
+    |> Enum.find(fn {_suffix, hash} ->
+      String.starts_with?(hash, required_start)
+    end)
+    |> elem(0)
+  end
+
+  def task_one do
+    find_advent_coin(@task_data)
+  end
 end
